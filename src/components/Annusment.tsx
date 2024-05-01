4e290c7fa3dfe2b359cd6cdef2af6b4739b23dbc
Aashish Negi <aashishnegi0000@gmail.com>
2024-05-01 11:41:52 +0530

    changed footer and amusement

--- src/components/Annusment.tsx
+++ src/components/Annusment.tsx
@@ -10,16 +10,16 @@ const Annusment = () => {
     return (
         <>
             <div className="flex justify-evenly py-10 mmm flex-row">
-                <div className={`w-[18vw] h-[10vw] rounded-[1.5vw] cursor-pointer flex justify-center items-center flex-col bg-[#7D55C7]`}><FaGears color='#fff' size={80} className='w-[14vw] h-[14vw] mx-2' />
-                    <p className='text-white font-semibold text-[2vw]'>UMS</p></div>
-                <div className={`w-[18vw] h-[10vw] rounded-[1.5vw] cursor-pointer flex justify-center items-center flex-col bg-[#7A838A]`}><BsPersonRaisedHand color='#fff' size={80} className='w-[14vw] h-[14vw] mx-2' />
-                    <p className='text-white font-semibold text-[2vw]'>Student Attendance</p></div>
-                <div className={`w-[18vw] h-[10vw] rounded-[1.5vw] cursor-pointer flex justify-center items-center flex-col bg-[#3DAB96]`}><IoBookSharp color='#fff' size={80} className='w-[14vw] h-[14vw] mx-2'/>
-                    <p className='text-white font-semibold text-[2vw]'>e-Library</p></div>
-                <div className={`w-[18vw] h-[10vw] rounded-[1.5vw] cursor-pointer flex justify-center items-center flex-col bg-[#EEAA43]`}><TiMessages color='#fff' size={80} className='w-[14vw] h-[14vw] mx-2'/>
-                    <p className='text-white font-semibold text-[2vw]'>Grievance</p></div>
-                <div className={`w-[18vw] h-[10vw] rounded-[1.5vw] cursor-pointer flex justify-center items-center flex-col bg-[#D24A87]`}><MdOutlineSportsKabaddi color='#fff' size={80} className='w-[14vw] h-[14vw] mx-2'/>
-                    <p className='text-white font-semibold text-[2vw]'>Hobby Club</p></div>
+                <a href="https://online.uktech.ac.in/account/ums-page.html"><div className={`w-[18vw] h-[10vw] rounded-[1.5vw] cursor-pointer flex justify-center items-center flex-col bg-[#7D55C7]`}><FaGears color='#fff' size={80} className='w-[14vw] h-[14vw] mx-2' />
+                    <p className='text-white font-semibold text-[2vw]'>UMS</p></div></a>
+                <a href="https://online.uktech.ac.in/ums/Student/Public/ViewDetail"><div className={`w-[18vw] h-[10vw] rounded-[1.5vw] cursor-pointer flex justify-center items-center flex-col bg-[#7A838A]`}><BsPersonRaisedHand color='#fff' size={80} className='w-[14vw] h-[14vw] mx-2' />
+                    <p className='text-white font-semibold text-[2vw]'>Student Attendance</p></div></a>
+                <a href="/"><div className={`w-[18vw] h-[10vw] rounded-[1.5vw] cursor-pointer flex justify-center items-center flex-col bg-[#3DAB96]`}><IoBookSharp color='#fff' size={80} className='w-[14vw] h-[14vw] mx-2' />
+                    <p className='text-white font-semibold text-[2vw]'>e-Library</p></div></a>
+                <a href="/"><div className={`w-[18vw] h-[10vw] rounded-[1.5vw] cursor-pointer flex justify-center items-center flex-col bg-[#EEAA43]`}><TiMessages color='#fff' size={80} className='w-[14vw] h-[14vw] mx-2' />
+                    <p className='text-white font-semibold text-[2vw]'>Grievance</p></div></a>
+                <a href="/"><div className={`w-[18vw] h-[10vw] rounded-[1.5vw] cursor-pointer flex justify-center items-center flex-col bg-[#D24A87]`}><MdOutlineSportsKabaddi color='#fff' size={80} className='w-[14vw] h-[14vw] mx-2' />
+                    <p className='text-white font-semibold text-[2vw]'>Hobby Club</p></div></a>
             </div>
 
         </>
--- src/components/Institute.tsx
+++ src/components/Institute.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Institute = () => {
     return (
         <>
-            <div className="py-4 px-8 flex flex-col  rounded-lg w-[40vw] gap-2 items-center border-4 border-red-500">
+            <div className="py-4 px-8 flex flex-col  rounded-lg lg:w-[44vw] w-[56vw] gap-2 items-center border-4 border-red-500">
                 <p className='text-[2vw] text-[#356EC8] font-bold'>About Institute </p>
                 <p className='text-[1.5vw]'>
                     Dr. A.P.J. Abdul Kalam Institute of Technology, Tanakpur is one of the campus institutes of UTU, Dehradun, established in 2016 by the Govt. of Uttarakhand to promote the technical education. The institute provides undergraduate education in two disciplines i.e. Mechanical Engineering and Civil Engineering intake of 60 in each branch. The vision of the institute is to produce a world class Technocrats and entrepreneurs for the benefit and upliftment of the society.Tanakpur is a town and a municipal board in Champawat district in the Indian state of Uttarakhand.
